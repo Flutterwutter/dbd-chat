a50ac7038d8584f109a6620f34215f0d5f9c4e80
Eros Camboni <Flutterwutter>
2025-11-08 23:47:51 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+const CHAT_PASSWORD = "Goldenfire2000"; // 🧙‍♂️ change this to your own secret phrase
+
+io.use((socket, next) => {
+    const clientPassword = socket.handshake.auth?.password;
+    if (clientPassword === CHAT_PASSWORD) {
+        next();
+    } else {
+        console.log("Rejected connection (wrong password)");
+        next(new Error("invalid password"));
+    }
+});
+
+io.on("connection", socket => {
+    console.log("✅ A player connected");
+    socket.on("chat", msg => io.emit("chat", msg));
+    socket.on("disconnect", () => console.log("❌ A player left"));
+});
+
+server.listen(8080, () => console.log("Chat running on http://localhost:8080"));
